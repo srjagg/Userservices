84622843ea6b7b63fb22210211a92a27c5dfac29
ingjohang <ingjohang@bythewave.co>
2023-01-31 18:31:27 -0500

    INSERT - Se crea la estructura del proyecto UserServices

---
+++ src/Services/UsuariosServices/UserServices.API/Controllers/UserServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserServices.API.Controllers
+{
+    public class UserServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
